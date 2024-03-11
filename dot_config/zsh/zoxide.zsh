56edbbfaf32d57863f2e1dcb158885a1a6182c6c
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-11 17:33:31 +0000

    upstream plugin now has support for override
    also for some reason prior to this change my tab completion wasn't working with `cd` but now it is? I dunno. either way, fixes 2 things :D

--- dot_config/zsh/zoxide.zsh
+++ dot_config/zsh/zoxide.zsh
@@ -1,7 +1,2 @@
-# I copied this from the oh-my-zsh plugin because I want to add --cmd to the init command
-
-if (( $+commands[zoxide] )); then
-  eval "$(zoxide init --cmd cd zsh)"
-else
-  echo '[oh-my-zsh] zoxide not found, please install it from https://github.com/ajeetdsouza/zoxide'
-fi
+plugins+=(zoxide)
+export ZOXIDE_CMD_OVERRIDE=cd
