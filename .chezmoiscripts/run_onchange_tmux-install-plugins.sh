4364a3c9d5fad866ef83c8699f1b254b66f6a03c
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-11 21:28:29 +0000

    install tmux plugins
    the plugin install script fails because reasons and I don't feel like
    debugging it anymore than I've already tried to so I'll just ignore the
    failure and move on.

---
+++ .chezmoiscripts/run_onchange_tmux-install-plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# this currently "fails" because it tries to install itself over itself and I started debugging why and decided I didn't really care
+~/.config/tmux/plugins/tpm/bin/install_plugins
+
+exit 0
